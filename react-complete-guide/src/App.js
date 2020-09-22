4d46c353986b081478c0cac9b91a30954d7982d3
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2020-09-22 14:16:41 -0600

    commit

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+
 class App extends Component {
   state = {
     persons: [
@@ -43,12 +44,17 @@ class App extends Component {
 
 
   render() {
-    const style = {
-      backgroundColor: 'white',
+    const buttonStyle = {
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -67,15 +73,30 @@ class App extends Component {
             })}
           </div>
       );
+
+      // buttonStyle.backgroundColor = 'red';
+      // buttonStyle[':hover'] = {
+      //   backgroundColor: 'lightred',
+      //   color: 'black'
+      // }
     };
+
+    let classes = [];
+    if (this.state.persons.length <=2) {
+      classes.push('red'); // classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold'); // classes = ['red', 'bold']
+    }
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App.</h1>
-        <p>This is really working!</p>
-        <button 
-        style={style} 
-        onClick={this.togglePersonsHandler}>Show People</button>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <button
+        className="button"
+        onClick={this.togglePersonsHandler}>Show People
+        </button>
         {persons}
       </div>
     );
